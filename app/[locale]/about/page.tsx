2d97673fdae6d9282cc0b75b9b8b29783e008441
Andres Guerra <andresguerra0625@gmail.com>
2026-06-01 14:30:31 -0500

    fix: cta colors

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -25,7 +25,7 @@ export default async function AboutPage() {
       <AboutValues />
       <AboutTeam />
       <CTA
-        bg="bg-[#F7EDD8]"
+        bg="bg-[#CBF6FF]"
         label={t("label")}
         title={t("title")}
         highlightWord={t("highlightWord")}
